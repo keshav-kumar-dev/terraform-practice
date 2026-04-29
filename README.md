f6221a0c7fc8c6942505b2243e19bb80398db32f
Keshav Kumar <keshav.kumar@unthinkable.co>
2026-04-29 12:42:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Terraform Practice
+
+This repository is for learning and practicing Terraform with AWS.
+
+## Current Resources
+- AWS VPC
+
+## How to use
+1. Install Terraform
+2. Configure AWS CLI
+3. Run `terraform init`
+4. Run `terraform plan`
+5. Run `terraform apply`
---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "project_name" {
+  description = "Project Name"
+  type        = string
+  default     = "terraform-project"
+}
